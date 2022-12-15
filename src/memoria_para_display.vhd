a9a4364f45f03167de3714cfbaee0d872fce45e0
Gustavo Sousa <gustavosousa@alu.ufc.br>
2022-12-15 15:53:15 -0300

    comentado codigo da memotia_para_display

--- src/memoria_para_display.vhd
+++ src/memoria_para_display.vhd
@@ -60,19 +60,8 @@ begin
 	div0 : divisor generic map (input_clock => spi_input_clock, output_clock => spi_input_clock)
     				port map (clk_in => f_clk_in, clk_out => clk_div);
 
-	sinal_atualizador : process(f_clk_in)
-		variable conta : integer := 1;
-	begin
-		if rising_edge(f_clk_in) then
-			if conta = 4 then
-				conta := 1;
-			else
-				conta := conta + 1;
-
-			end if;
-		end if;
-	end process;
-
+	-- maquina de estados atualizada o mais rapido possivel
+	-- evita depender do clock do divisor e ter mais tempo por causa de intervalos
 	passador_spi: process (f_clk_in) 
 	begin
 		if rising_edge(f_clk_in) then
@@ -80,6 +69,7 @@ begin
 		end if;
 	end process;
 
+	-- registrador de uso unico para enviar comando de escrita
     iniciar_comando_de_escrita_de : process (comando_escreve, comando)
 	begin
 		if rising_edge(comando_escreve) then
@@ -89,6 +79,7 @@ begin
 		end if;
 	end process;
 	
+	-- o mesmo para os dados apos envio do comando
 	iniciar_escrita_pixels : process (comando_terminado, GO, f_clk_in)
 	begin
         if rising_edge(f_clk_in) then
@@ -105,18 +96,24 @@ begin
 	begin
 		if falling_edge(clk_div) then
 			if comando = '1' then
+				-- isso e um comando
 				s_dc <= '0';
+				-- habilita saido do clock
 				spi_out_enable_command <= '1';
 				
+				-- evita erro de indice 
 				if conta_comando > 0 then
 					s_mosi_comando <= constante_comando_escrever_vram(conta_comando - 1);
 				end if;
 				
+
 				if conta_comando = 0 then
+					-- chegando em zero preparamos para enviar os pixels
 					s_dc <= '1';
-					spi_out_enable_command <= '0';
-                    comando_terminado <= '1';
+					spi_out_enable_command <= '0'; -- desabilita saida do clock
+                    comando_terminado <= '1'; -- sinaliza fim do comando
 				else 
+					-- decrementa contador
 					conta_comando := conta_comando - 1;
 				end if;
 			end if;
@@ -129,6 +126,7 @@ begin
 		variable p1, p2, p3 : std_logic_vector(7 downto 0) := (others => '0');
 	begin
 		if falling_edge(clk_div) then
+			-- separa 18 bits em 3 bytes de 6 bits + 00
             p1 := s_flash_data(17 downto 12) & "00";
 			p2 := s_flash_data(11 downto 6) & "00";
 			p3 := s_flash_data(5 downto 0) & "00";
@@ -136,32 +134,38 @@ begin
 			if GO = '1' then -- and lock = '0' then
 				case estado_atual is
 				when parado =>
-					-- aqui era usado enable = '0' para iniciar uma transmissao, vou deixar automatico
+					-- sempre que enable = 1 transmitimos, isso depende do top
 					if enable = '1' then
 						estado <= enviando;
 					else
 						estado <= estado;
 					end if;
 				when enviando =>
+					-- habilita saida do clock
 					spi_out_enable_pixels <= '1';
 					
+					-- evita erro de indice, seleciona o byte a ser enviado, envia bit a bit dele
 					if conta_local > 0 then
 						case conta_byte is
-						when 1 =>
+						when 1 => -- R
 							s_mosi_pixels <= p1(conta_local - 1);
-						when 2 =>
+						when 2 => -- G
 							s_mosi_pixels <= p2(conta_local - 1);
-						when 3 =>
+						when 3 => -- B
 							s_mosi_pixels <= p3(conta_local - 1);
 						end case;
 					end if;
 
 					if conta_local = 0 then
+						-- enquanto preparamos o proximo byte, desabilitamos a saida do clock
 						spi_out_enable_pixels <= '0';
 						conta_local := 8;
                         if conta_byte < 3 then
+							-- conta_local zerou, proximo byte
                             conta_byte := conta_byte + 1;
                         else
+							-- redefine conta_byte
+							-- aumenta coluna 240 por 320 linhas
                             conta_byte := 1;
                             if coluna < 239 then
                                 coluna <= coluna + 1;
@@ -170,6 +174,7 @@ begin
 								if linha < 319 then
 									linha <= linha + 1;
 								else 
+									-- ao acabar linhas e colunas, volta ao estado parado e reseta variaveis
 									estado <= parado;
 									conta_byte := 1;
 									conta_local := 8;
@@ -179,6 +184,7 @@ begin
                             end if;
                         end if;
 					else
+						-- decrementa bit a ser mandado
 						conta_local := conta_local - 1;
 					end if;
 				end case;
@@ -186,26 +192,34 @@ begin
 		end if;
 	end process;
 	
+	-- a ideia era usar memoria flash, no entando os sinais sao similares às memórias pROM da Gowin
 	flash_oe <= '0';
 	flash_we <= '1';
 	flash_reset <= '1';
 	
+	-- sinais foram definidos,depois associados as entradas e saidas, sempre permitindo existir esse 
+	-- sumário no final com informações importantes
+
+	-- arbitragem de bus para cada processo
 	f_cs <= '0' when estado_atual = enviando or comando_terminado = '0' else '1';
-	
 	f_dc <= s_dc;
-	
 	f_mosi <= s_mosi_pixels when (estado = enviando and spi_out_enable_command = '0') else
 				s_mosi_comando when spi_out_enable_command = '1' else '0';
-				
 	f_sck <= clk_div when (estado = enviando and spi_out_enable_pixels = '1') or spi_out_enable_command = '1' else '0';
 
-	s_flash_data <= flash_data;
+	-- maior segredo do codigo, escrevemos menos pixels em mais pixels
+	s_flash_data <= flash_data; --recebemos da entrada de dados da memoria flash
+	-- o endereço tem uma escala para zoom e offset para movimentação
+	-- como vhdl vai arredondar para baixo, para sair de 0 a 1 numa escala de 2, temos que contar dois endereços
+	-- isso faz o zoom
 	flash_address <= std_logic_vector(to_unsigned(((coluna/escala) + s_offset_x) + 120*((linha/escala) + s_offset_y), 15));
 
     s_entrada_fim_jogo <= entrada_fim_jogo;
 
+	-- seletor de escala com base no fim de jogo
     escala <= 2 when (s_entrada_fim_jogo = '1') else 16;
 
+	-- seletor de offset com base no fim de jogo, recebendo entrada de offset diretamente do top
     s_offset_x <= input_offset_x when s_entrada_fim_jogo = '0' else 0;
     s_offset_y <= input_offset_y when s_entrada_fim_jogo = '0' else 0;
 
